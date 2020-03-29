f7a1b83bb92377ad27bdb1fc7d7ae54fce6614a9
puckish-system-8282 <bryce.boddy@gmail.com>
2020-03-29 22:11:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,14 @@
 def unsafe?(speed)
-  if speed < 40 || speed > 60
+  if speed < 40 && speed > 60
     return true
-  else return false
+  else 
+    return false
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 || speed > 60 ? return true : return false
+	speed < 40 && speed > 60 ? return true : return false
 end
 	
 
